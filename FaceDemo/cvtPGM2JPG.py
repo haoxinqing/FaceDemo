9b616e9279b9a589575b3b9aa0f0ecca43cf6e2e
wqdonald <wqdonald@163.com>
2019-06-11 22:55:59 +0800

    add ORL face database download from website, and check file's integrality by using MD5 hash

--- FaceDemo/cvtPGM2JPG.py
+++ FaceDemo/cvtPGM2JPG.py
@@ -1,11 +1,51 @@
 import os
 import numpy as np
 import cv2 as cv
+import urllib.request
+import zipfile as zip
+import hashlib as hash
 
 pgmset_path = os.path.curdir + '/../' + 'att_faces/'
 jpgset_path = os.path.curdir + '/' + 'att_faces_jpg/'
 if False == os.path.exists(jpgset_path):
     os.mkdir(jpgset_path)
+
+# Download ORL face database from web
+if False == os.path.exists(pgmset_path):
+    os.mkdir(pgmset_path)
+
+    temp_path = os.path.curdir + '/../' + 'temp/'
+    zip_file = temp_path + 'att_faces.zip'
+    if True == os.path.exists(zip_file):
+        # check file hash correct code is 'ea6281c63bb2e68a64c046957a749086'
+        hashcode = 'ea6281c63bb2e68a64c046957a749086'
+        try:
+            f = open(zip_file, 'rb')
+            bytes = f.read()
+        finally:
+            if f:
+                f.close()
+        code = hash.md5()
+        code.update(bytes)
+        hex = code.hexdigest()
+        print(hex)
+        if hex != hashcode:
+            # not match, delete it
+            os.remove(zip_file);
+
+    if False == os.path.exists(zip_file):
+        if False == os.path.exists(temp_path):
+            os.mkdir(temp_path)
+        # download ORL
+        download_url = 'http://www.cl.cam.ac.uk/Research/DTG/attarchive/pub/data/att_faces.zip'
+        zip_file = temp_path + 'att_faces.zip'
+        urllib.request.urlretrieve(download_url, zip_file);
+    # extract zip file
+    f = zip.ZipFile(zip_file, 'r')
+    for file in f.namelist():
+        f.extract(file, pgmset_path)
+    f.close()
+
 
 dirs = os.listdir(pgmset_path)
 for item in dirs:
